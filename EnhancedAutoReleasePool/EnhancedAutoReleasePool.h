30f5f29028af6acabc3c48b7ebbe090f5abf932a
sgr-ksmt <melodydance.k.s@gmail.com>
2016-02-12 00:09:10 +0900

    Add functions

---
+++ EnhancedAutoReleasePool/EnhancedAutoReleasePool.h
@@ -0,0 +1,19 @@
+//
+//  EnhancedAutoReleasePool.h
+//  EnhancedAutoReleasePool
+//
+//  Created by Suguru Kishimoto on 2016/02/11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EnhancedAutoReleasePool.
+FOUNDATION_EXPORT double EnhancedAutoReleasePoolVersionNumber;
+
+//! Project version string for EnhancedAutoReleasePool.
+FOUNDATION_EXPORT const unsigned char EnhancedAutoReleasePoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnhancedAutoReleasePool/PublicHeader.h>
+
+
---
+++ EnhancedAutoReleasePool/EnhancedAutoReleasePool.swift
@@ -0,0 +1,48 @@
+//
+//  EnhancedAutoReleasePool.swift
+//  EnhancedAutoReleasePool
+//
+//  Created by Suguru Kishimoto on 2016/02/11.
+//
+//
+
+import Foundation
+
+
+public func autoreleasepool(@noescape code: () throws -> ()) rethrows {
+    try {
+        var error: ErrorType?
+        autoreleasepool {
+            do {
+                try code()
+            } catch (let e) {
+                error = e
+            }
+        }
+        if let error = error {
+            throw error
+        }
+    }()
+}
+
+public func autoreleasepool<V>(@noescape code: () throws -> V?) rethrows -> V? {
+    return try {
+        var value: V?
+        var error: ErrorType?
+        autoreleasepool {
+            do {
+                value = try code()
+            } catch (let e) {
+                error = e
+            }
+        }
+        if let error = error {
+            throw error
+        }
+        return value
+    }()
+}
+
+public func autoreleasepool<V>(@noescape code: () throws -> V) rethrows -> V {
+    return try autoreleasepool(code)!
+}
